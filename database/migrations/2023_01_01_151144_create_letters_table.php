a42a21406bb6bb3522572f306da23003b34a94ec
gioanbatan <giovannibassani@hotmail.com>
2023-01-01 16:42:52 +0100

    First Commit: laravel base and migration

---
+++ database/migrations/2023_01_01_151144_create_letters_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('letters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname', 100);
+            $table->string('address', 100);
+            $table->string('city');
+            $table->date('arrive_date');
+            $table->string('gift', 255);
+            $table->text('mail_text');
+            $table->tinyInteger('rating');
+            $table->boolean('delivered');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('letters');
+    }
+};
